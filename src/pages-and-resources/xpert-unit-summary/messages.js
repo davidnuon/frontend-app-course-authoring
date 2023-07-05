e94eb5c41c7a3b1dcd0cf92d271ebfdb052656d2
David Nuon <davidnuongm@gmail.com>
2023-07-05 13:02:13 -0700

    Update message strings for modal

--- src/pages-and-resources/xpert-unit-summary/messages.js
+++ src/pages-and-resources/xpert-unit-summary/messages.js
@@ -2,22 +2,21 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 
 const messages = defineMessages({
   heading: {
-    id: 'course-authoring.pages-resources.calculator.heading',
-    defaultMessage: 'Configure Xpert Unit Summaries',
+    id: 'course-authoring.pages-resources.xpert-unit-summary.heading',
+    defaultMessage: 'Configure Xpert unit summaries',
   },
   enableCalculatorLabel: {
-    id: 'course-authoring.pages-resources.calculator.enable-calculator.label',
-    defaultMessage: 'Xpert Unit Summaries',
+    id: 'course-authoring.pages-resources.xpert-unit-summary.enable-xpert-unit-summary.label',
+    defaultMessage: 'Xpert unit summaries',
   },
   enableCalculatorHelp: {
-    id: 'course-authoring.pages-resources.calculator.enable-calculator.help',
-    defaultMessage: `The calculator supports numbers, operators, constants,
-      functions, and other mathematical concepts. When enabled, an icon to
-      access the calculator appears on all pages in the body of your course.`,
+    id: 'course-authoring.pages-resources.xpert-unit-summary.enable-xpert-unit-summary.help',
+    defaultMessage: `Enable concise summaries of text and video content. Control the availability of summaries in the settings dialog for each unit.
+    `,
   },
   enableCalculatorLink: {
-    id: 'course-authoring.pages-resources.calculator.enable-calculator.link',
-    defaultMessage: 'Learn more about the calculator',
+    id: 'course-authoring.pages-resources.xpert-unit-summary.enable-xpert-unit-summary.link',
+    defaultMessage: 'Learn more about the Xpert unit summaries',
   },
 });
 
